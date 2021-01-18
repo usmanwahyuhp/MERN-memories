18d06504e794775976ca4ddfb94cb24d1defca5e
usmanwahyuhp <usmanwahyuhp@gmail.com>
2021-01-18 21:25:24 +0700

    Redux working

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.use('/posts', postRoutes);
 // mongodb+srv://
 const CONNECTION_URL = 'mongodb+srv://usmanwahyu:usmanwahyu123@cluster0.shwh9.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
