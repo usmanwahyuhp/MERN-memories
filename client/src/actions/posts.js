3333550b869285eeb1b0a5eb56a8791c7e5a8784
usmanwahyuhp <usmanwahyuhp@gmail.com>
2021-01-18 20:26:44 +0700

    Using redux thunk to dealing with async dispatch function

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,8 +1,7 @@
 import * as api from '../api';
 
-//Action Creator : is a function that return an action and an action is just an object that has a type and payload, with redux thunk since we'll be dealing  with asynchronous we need to add dispatch function in front of it and then instead returning an action we have to dspatch it
-const getPosts = () => {
+const getPosts = () => async (dispatch) => {
     const action = { type: 'FETCH_ALL', payload: [] }
 
-    return action;
+    dispatch(action);
 }
