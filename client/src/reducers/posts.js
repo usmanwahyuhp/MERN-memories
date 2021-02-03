78595745ed886d5313c185207a85309266c3308b
usmanwahyuhp <usmanwahyuhp@gmail.com>
2021-02-03 22:35:52 +0700

    Like Functionality

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -7,6 +7,7 @@ let posts = (posts = [], action) => {
         case 'CREATE':
             return [...posts, action.payload];
         case 'UPDATE':
+        case 'LIKE':
             return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
         default:
             return posts;
