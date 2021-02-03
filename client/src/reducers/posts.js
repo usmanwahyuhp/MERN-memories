971acc6f3ba8a510216f23a1eaebecd3daf07a75
usmanwahyuhp <usmanwahyuhp@gmail.com>
2021-02-03 16:37:55 +0700

    debug

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,4 +1,4 @@
-export default (posts = [], action) => {
+let posts = (posts = [], action) => {
     switch(action.type){
         case 'FETCH_ALL':
             return action.payload;
@@ -10,3 +10,5 @@ export default (posts = [], action) => {
             return posts;
     }
 }
+
+export default posts;
