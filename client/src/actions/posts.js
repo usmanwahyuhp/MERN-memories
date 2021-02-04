572d8ab31e7cf72394053fadfeaad7200f929339
usmanwahyuhp <usmanwahyuhp@gmail.com>
2021-02-04 17:22:43 +0700

    Action Constants

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,12 @@
 import * as api from '../api/index.js';
+import { CREATE, DELETE, FETCH_ALL, LIKE, UPDATE } from '../constants/actionTypes.js';
+
 
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
     
-        dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({ type: FETCH_ALL, payload: data });
     } catch (error) {
         console.log(error.message);
     }
@@ -14,7 +16,7 @@ export const createPost = (post) => async (dispatch) => {
     try {
       const { data } = await api.createPost(post);
   
-      dispatch({ type: 'CREATE', payload: data });
+      dispatch({ type: CREATE, payload: data });
     } catch (error) {
       console.log(error.message);
     }
@@ -24,7 +26,7 @@ export const createPost = (post) => async (dispatch) => {
     try {
       const { data } = await api.updatePost(id, post);
   
-      dispatch({ type: 'UPDATE', payload: data });
+      dispatch({ type: UPDATE, payload: data });
     } catch (error) {
       console.log(error.message);
     }
@@ -34,7 +36,7 @@ export const createPost = (post) => async (dispatch) => {
     try {
       await api.deletePost(id);
   
-      dispatch({ type: 'DELETE', payload: id });
+      dispatch({ type: DELETE, payload: id });
     } catch (error) {
       console.log(error.message);
     }
@@ -44,7 +46,7 @@ export const createPost = (post) => async (dispatch) => {
     try {
       const { data } = await api.likePost(id);
   
-      dispatch({ type: 'LIKE', payload: data });
+      dispatch({ type: LIKE, payload: data });
     } catch (error) {
       console.log(error.message);
     }
