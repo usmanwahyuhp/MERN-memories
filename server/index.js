b871bd0dd7ad2ec679efc790db9a8d8bc0e4479a
usmanwahyuhp <usmanwahyuhp@gmail.com>
2021-02-04 22:46:29 +0700

    Environmental Variables

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -14,10 +16,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 // mongodb+srv://
-const CONNECTION_URL = 'mongodb+srv://usmanwahyu:usmanwahyu123@cluster0.shwh9.mongodb.net/<dbname>?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+// const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
